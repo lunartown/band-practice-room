31331b6e290bf6c38f81aed971d22119981b88f6
lunartown <lunartown@gmail.com>
2026-06-25 09:20:57 +0900

    CHORE: 모바일 웹 상단 여백 변경 롤백

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
-import { Capacitor } from '@capacitor/core';
 import { App } from './App';
 import { initFavorites } from './lib/favorites';
 import { notifyLiveUpdateReady } from './lib/liveUpdate';
 import './styles.css';
-
-document.documentElement.dataset.runtime = Capacitor.isNativePlatform() ? 'native' : 'web';
 
 // 저장된 즐겨찾기를 기기에서 미리 불러온다(렌더 전에 시작, 완료되면 구독자에 반영).
 void initFavorites();
